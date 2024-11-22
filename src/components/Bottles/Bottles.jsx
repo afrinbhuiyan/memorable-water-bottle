ab3ba1027f0c779378395e48d0eeb87c16fd777e
afrinbhuiyan <mstafrinbhuiyan@gmail.com>
2024-11-23 00:19:50 +0600

    proptypes warning and remove from local storage and card

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Bottle from "../Bottle/Bottle";
 import "./Bottles.css"
-import { addToLS, getStoredCart } from "../../utilities/localStorage";
+import { addToLS, getStoredCart, removeFromLS } from "../../utilities/localStorage";
 import Cart from "../Cart/Cart";
 
 const Bottles = () => {
@@ -44,10 +44,18 @@ const [cart, setCart] = useState([]);
             addToLS(bottle.id)
         }
 
+        const handleRemoveFromCart = id => {
+            // visual cart remove
+            const remainingCart = cart.filter( bottle => bottle.id !== id);
+            setCart(remainingCart);
+            // remove from LS
+            removeFromLS(id);
+        }
+
     return (
         <div>
             <h2>Bottles Availble : {bottles.length} </h2>
-            <Cart cart={cart} ></Cart>
+            <Cart cart={cart} handleRemoveFromCart={handleRemoveFromCart} ></Cart>
             <div className="bottle-container">
             {
                 bottles.map(bottle => <Bottle
